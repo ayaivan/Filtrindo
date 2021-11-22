d388541838b50239b12f5d8e89160e97229e5d4d
ayaivan <falconeagle70@yahoo.com>
2021-11-22 19:32:30 +0700

    Initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '2c306ee0922555fede1630c119db1566'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '5b98ad52-cba2-48a5-a126-fad837df6ee3'
+};
