a56b05a4395dd83aa963bff3798b623deefb5d1b
rezalimena <64654679+rezalimena@users.noreply.github.com>
2021-12-18 01:41:33 +0700

    restructure JSON

--- api/purchase-lpb/controllers/purchase-lpb.js
+++ api/purchase-lpb/controllers/purchase-lpb.js
@@ -8,13 +8,13 @@
 module.exports = {
   async create(ctx) {
     const result = await strapi.services["purchase-lpb"].create(ctx.request.body.header);
-    await strapi.services["purchase-lpb"].updatePOInsert(ctx.request.body.detail, ctx.request.body.header, result);
+    await strapi.services["purchase-lpb"].updateLPB(ctx.request.body.detail, ctx.request.body.header, result, 'post');
   
     return result;
   },
   async update(ctx) {
     const result = await strapi.services["purchase-lpb"].update({id:ctx.params.id},ctx.request.body.header);
-    await strapi.services["purchase-lpb"].updatePOUpdate(ctx.request.body.detail, ctx.request.body.header, ctx.params.id);
+    await strapi.services["purchase-lpb"].updateLPB(ctx.request.body.detail, ctx.request.body.header, result, 'put');
     return result;
   },
   async saveOrder(ctx) {
--- api/purchase-lpb/services/purchase-lpb.js
+++ api/purchase-lpb/services/purchase-lpb.js
@@ -356,7 +356,7 @@ module.exports = {
           nama: "HPP",
         });
         hppCode = hppCode.akunting;
-        console.log(headerLPB.gudang)
+        console.log(headerLPB.gudang);
         const warehouseCode = headerLPB.gudang.akunting;
         const bukuBesarResponse = [];
         const pembelianBukuBesar = {
@@ -430,7 +430,7 @@ module.exports = {
         order.idBukuBesar = bukuBesarResponse.map((bukuBesar) => bukuBesar.id);
       }
 
-      if(!order.isDeleted) updatedListOrder.push(order);
+      if (!order.isDeleted) updatedListOrder.push(order);
     }
     const orderPayLoad = updatedListOrder.map((order) => ({
       idPO: order.idPO,
@@ -443,6 +443,173 @@ module.exports = {
       { id: idLPB },
       { order_list: orderPayLoad }
     );
+    return JSON.stringify("sukses");
+  },
+
+  async updateLPB(listOrder, headerLPB, purchase, mode) {
+    const updatedListOrder = [];
+    const mutasiBarangDetails = [];
+    const bukuBesarDetails = [];
+
+    // buku besar
+    let purchaseCode = await strapi.services["akuntansi-mapping"].findOne({
+      nama: "Pembelian",
+    });
+    purchaseCode = purchaseCode.akunting;
+    let debtCode = await strapi.services["akuntansi-mapping"].findOne({
+      nama: "Hutang dagang pembelian",
+    });
+    debtCode = debtCode.akunting;
+    let hppCode = await strapi.services["akuntansi-mapping"].findOne({
+      nama: "HPP",
+    });
+    hppCode = hppCode.akunting;
+    const warehouseCode = headerLPB.gudang.akunting;
+    const accountingListCode = [
+      purchaseCode.id,
+      debtCode.id,
+      warehouseCode,
+      hppCode.id,
+    ];
+
+    for (let index = 0; index < listOrder.length; index++) {
+      const order = listOrder[index];
+      // update PO
+      if (order.idPO !== null) {
+        // find PO on table PO
+        const listOrderPO = await strapi.services["h-purchase-order"].findOne({
+          id: order.idPO,
+        });
+
+        // find selected item
+        let selectedItem = listOrderPO.listOrder.filter(
+          (item) => item.id === order.item.id
+        );
+        if (selectedItem.length > 0) {
+          if (!order.isDeleted) {
+            if (order.oldData.jumlah > order.item.jumlah)
+              selectedItem[0].realisasi -= parseFloat(
+                order.oldData.jumlah - order.item.jumlah
+              );
+            else if (order.oldData.jumlah < order.item.jumlah)
+              selectedItem[0].realisasi += parseFloat(
+                order.item.jumlah - order.oldData.jumlah
+              );
+          } else selectedItem[0].realisasi -= parseFloat(order.oldData.jumlah);
+        }
+
+        // determine if realisasi - jumlah <= 0 then isEmpty = true
+        const diffCount = listOrderPO.listOrder
+          .map((order) => order.jumlah - order.realisasi)
+          .reduce((prev, curr) => prev + curr);
+
+        let isEmpty;
+        if (diffCount <= 0) isEmpty = true;
+        else isEmpty = false;
+
+        await strapi.services["h-purchase-order"].update(
+          { id: order.idPO },
+          { listOrder: listOrderPO.listOrder, isEmpty }
+        );
+      }
+      // preparing detail for mutasi barang
+      if (!order.isDeleted)
+        mutasiBarangDetails.push({
+          idBarang: order.item.id,
+          masuk: order.item.jumlah,
+          keluar: 0,
+          hargaDolar: order.item.harga_dolar,
+          hargaRupiah: order.item.harga_rupiah,
+        });
+
+      // detail stok
+      const objItem = await strapi.services["detail-stok-barang"].findOne({
+        barang: order.item.id,
+      });
+      const stokGudang = objItem.detail.filter(
+        (item) => item.id === headerLPB.gudang.id
+      );
+      if (stokGudang.length > 0)
+        stokGudang[0].stok_A += parseFloat(order.item.jumlah);
+      const stokResponse = await strapi.services["detail-stok-barang"].update(
+        { barang: order.item.id },
+        { detail: objItem.detail }
+      );
+
+      // preparing detail buku besar
+      if (!order.isDeleted) {
+        bukuBesarDetails.push({
+          entity_1: {
+            idAkunting: purchaseCode.id,
+            idBarang: order.item.id,
+            debet: order.item.harga_rupiah * order.item.jumlah,
+            kredit: 0,
+          },
+          entity_2: {
+            idAkunting: debtCode.id,
+            idBarang: order.item.id,
+            debet: 0,
+            kredit: order.item.harga_rupiah * order.item.jumlah,
+          },
+          entity_3: {
+            idAkunting: warehouseCode.id,
+            idBarang: order.item.id,
+            debet: order.item.harga_rupiah * order.item.jumlah,
+            kredit: 0,
+          },
+          entity_4: {
+            idAkunting: hppCode.id,
+            idBarang: order.item.id,
+            debet: 0,
+            kredit: order.item.harga_rupiah * order.item.jumlah,
+          },
+        });
+      }
+    }
+    // mutasi stok
+    const payLoadMutasi = {
+      barangs: headerLPB.barangs,
+      tanggal: headerLPB.tanggal_lpb,
+      kurs: headerLPB.kurs,
+      asal: "PBeli",
+      gudang: headerLPB.gudang.id,
+      surat: headerLPB.nota,
+      createdBy: headerLPB.createdBy,
+      updatedBy: headerLPB.updatedBy,
+      purchase_lpb: purchase.id,
+      details: mutasiBarangDetails,
+    };
+    if (mode === "put")
+      await strapi.services["mutasi-barang"].update(
+        { id: purchase.mutasi_barang.id },
+        payLoadMutasi
+      );
+    else if (mode === "post")
+      await strapi.services["mutasi-barang"].create(payLoadMutasi);
+
+    // buku besar
+    const payLoadBukuBesar = {
+      tanggal: headerLPB.tanggal_lpb,
+      surat: headerLPB.nota,
+      kode_bank: null,
+      jenis: "PBeli",
+      keterangan: null,
+      createdBy: headerLPB.createdBy,
+      updatedBy: headerLPB.updatedBy,
+      akuntings: accountingListCode,
+      purchase_lpb: purchase.id,
+      customer: null,
+      supplier: headerLPB.supplier,
+      details: bukuBesarDetails,
+    };
+    if (mode === "put")
+      await strapi.services["akunting-buku-besar"].update(
+        { id: purchase.akunting_buku_besar.id },
+        payLoadBukuBesar
+      );
+    else if (mode === "post")
+      await strapi.services["akunting-buku-besar"].create(payLoadBukuBesar);
+
     return JSON.stringify("sukses");
   },
 };
